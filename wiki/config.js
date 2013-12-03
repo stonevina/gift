8d49f5bb006775de4d6a6f62409ba850252bbfe4
woshiwanting <1370348686@qq.com>
2013-12-03 17:50:19 +0800

    增加MD编辑工具

--- wiki/config.js
+++ wiki/config.js
@@ -85,11 +85,12 @@
                 'http://it-ebooks.info/', 'http://codepen.io/', 'http://codevisually.com/',
                 'http://css3lib.alloyteam.com/', 'http://html5demos.com/',
                 'http://devdocs.io/', 'http://tool.css-js.com/','http://jinlong.github.io/blog/2013/08/29/devtoolsecrets/',
-                'http://hotoo.googlecode.com/svn/trunk/labs/css/css-fonts.html'],
+                'http://hotoo.googlecode.com/svn/trunk/labs/css/css-fonts.html',
+                'https://stackedit.io/'],
                 text : ['mdeditor', 'Java', 'JavaScript', 'CSS', 'Webkitcss', 'FElab', 'TB', 'HTML',
                 'ActionScript', 'FreeBook', 'Runjs', 'jsbin', 'jsfiddle', 'gopng', 'tinypng', 'native2ascii', 'nodeschool', 'teamtreehouse',
                 'tutorials', 'api', 'FESource', 'it-ebooks', 'codepen', 'codevisually', 'alloyteam', 
-                'html5demos', 'devDoc', 'jsmin','devTool','css-fonts']
+                'html5demos', 'devDoc', 'jsmin','devTool','css-fonts','stackedit']
             }
         },{
             title : '干货分享',
